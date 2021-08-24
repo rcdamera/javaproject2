0b0734835544387aee50b3a54404b8d0fc8cc9e4
rcdamera <rcdamera@gmail.com>
2021-08-24 18:31:42 +0530

    this for testing456

--- gitTest2-Branch_testing/src/com/geetuapp/gitTest2Branch.java
+++ gitTest2-Branch_testing/src/com/geetuapp/gitTest2Branch.java
@@ -5,6 +5,7 @@ public class gitTest2Branch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//This is from Ram Branch12 eclipse
 		System.out.println("Hello Ram 5467");
 		 
 		 //This is for change in second system 
@@ -12,6 +13,7 @@ public class gitTest2Branch {
 		// this is for ram branch 1
 		
 		//This is from Ram Branch1 eclipse
+		System.out.println("Hello Ram 1234");
 
 	}
 
